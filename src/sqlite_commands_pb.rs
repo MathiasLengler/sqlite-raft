2d8d9bcb3925ea5ed438f3125b3a4893499cec6c
Mathias Lengler <mathmatze@googlemail.com>
2018-09-10 04:01:43 +0200

    add TODOs

--- src/sqlite_commands_pb.rs
+++ src/sqlite_commands_pb.rs
@@ -2,6 +2,11 @@ use proto_gen::raftsqlite::ProtoQueryRequest;
 use sqlite_commands::query::Query;
 use sqlite_commands::parameter::QueuedParameters;
 
+// TODO: implement the protobuf layer inside sqlite-commands crate because of visibility.
+// TODO: inject generated protobuf code inside this crate.
+// TODO: implement skeleton of RaftNode and RaftSqliteClientAPI
+// TODO: decide where/how to implement the "Node communication trait" for the grpc RaftNode service
+
 impl From<Query> for ProtoQueryRequest {
     fn from(query: Query) -> Self {
         let mut proto_query_request = ProtoQueryRequest::new();

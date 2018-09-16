a2c5c1e8636abad789f4544aa079fdf6d1654a1e
Mathias Lengler <mathmatze@googlemail.com>
2018-09-16 03:31:29 +0200

    simplify sqldiff assert

--- tests/utils/sqldiff.rs
+++ tests/utils/sqldiff.rs
@@ -6,8 +6,8 @@ use std::process::Output;
 pub fn assert_db_eq(test_db_path: &Path, expected_db_path: &Path) {
     let output = exec_sqldiff(test_db_path, expected_db_path);
 
-    assert_eq!(output.stdout.len(), 0, "Unexpected sqldiff stdout content: {:?}", output);
-    assert_eq!(output.stderr.len(), 0, "Unexpected sqldiff stderr content: {:?}", output);
+    assert!(output.stdout.is_empty(), "Unexpected sqldiff stdout content: {:?}", output);
+    assert!(output.stderr.is_empty(), "Unexpected sqldiff stderr content: {:?}", output);
 }
 
 fn exec_sqldiff(test_db_path: &Path, expected_db_path: &Path) -> Output {

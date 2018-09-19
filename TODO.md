3a3592370455d6346e7535e70cdadba7996c4068
Mathias Lengler <mathmatze@googlemail.com>
2018-09-19 02:53:56 +0200

    update TODOs

--- TODO.md
+++ TODO.md
@@ -1,3 +1,5 @@
+rename raftsqlite -> sqlite-raft
+
 implement skeleton of RaftNode and RaftSqliteClientAPI
 
 decide where/how to implement the "Node communication trait" for the grpc RaftNode service
--- src/bin/raftsqlite.rs
+++ src/bin/raftsqlite.rs
@@ -5,21 +5,8 @@ extern crate grpc_experiments;
 /// Timeout behaviour
 /// Propose a sql command -> get back response from node
 ///
-/// ## API
-///
-/// `run(enum SqliteRequest) -> ?`
-///
-/// vs
-///
-/// Separate methods for each command type:
-/// - Query
-/// - Execute
-/// - BulkQuery
-/// - BulkExecute
-///
 /// # Node <-> Node
 /// Pass raft-rs messages
-/// Bidirectional / Two send endpoints
 ///
 /// ## Testing
 /// Communication Trait (for channel transport)
@@ -30,22 +17,6 @@ extern crate grpc_experiments;
 /// client request -> raft propose -> wait for raft commit -> find callback -> complete client request
 ///
 /// Must bridge Future/Raft stepping API
-///
-/// # sqlite-commands
-/// - port data structures to protobuf
-///     - How to add functionality to generated structs?
-///     - sqlite-commands already implemented in rust
-/// - Custom wrapper around serde payload
-///     - use gRPC transport infrastructure (timeout, compression, ...)
-///     - NodeToNode as bincode, ClientToNode as JSON
-///     - mixed client experience
-///         - has to generate JSON and gRPC
-///         - plain JSON better? HTTP Server?
-///         - bad for bandwidth/performance
-/// - Hybrid
-///     - port only the API layer for Clients
-///         - big percentage of sqlite-commands
-///     - mirror API and provide from impls
 
 extern crate grpcio;
 #[macro_use]

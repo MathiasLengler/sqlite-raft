15629a1bbe6647589a689bbf94cd0efb0dc31b74
Mathias Lengler <mathmatze@googlemail.com>
2018-10-05 15:37:45 +0200

    add TODOs

---
+++ tests/tests.rs
@@ -0,0 +1,5 @@
+extern crate model;
+
+// TODO: refactor MemStorage tests to run against the Storage traits
+// TODO: run test against both storage implementations
+// TODO: implement model test Mem vs Sqlite storage

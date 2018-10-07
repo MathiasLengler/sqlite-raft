495bb81d97cdd5fcf742c904734d792470f0fa18
Mathias Lengler <mathmatze@googlemail.com>
2018-10-07 16:52:06 +0200

    WIP port mem storage tests

--- tests/tests.rs
+++ tests/tests.rs
@@ -1,4 +1,9 @@
 extern crate model;
+extern crate raft;
+extern crate protobuf;
+extern crate sqlite_raft_storage;
+
+mod mem_storage_tests;
 
 // TODO: refactor MemStorage tests to run against the Storage traits
 // TODO: run test against both storage implementations
